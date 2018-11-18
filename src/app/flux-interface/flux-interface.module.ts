6212ad9ecd4bae94d23ec751fd77fbcc6b226505
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2018-11-17 19:22:02 -0500

    Apply material design lib to flux-interface

--- src/app/flux-interface/flux-interface.module.ts
+++ src/app/flux-interface/flux-interface.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FluxInterfaceComponent } from './flux-interface.component';
 import { FluxDialogComponent } from './flux-dialog/flux-dialog.component';
+import { MaterialModule } from '../material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   declarations: [
     FluxInterfaceComponent,
