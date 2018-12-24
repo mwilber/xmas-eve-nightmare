fe91c1ecb4f248396ce6c250815cbc294d8942d5
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2018-12-23 21:46:40 -0500

    Add cache versioning to serviceworker

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -1,18 +1,32 @@
+var CACHE_STATIC_NAME = 'static-v3';
+
+var STATIC_CACHE_REF = [
+    '/',
+    '/index.html',
+];
+
 self.addEventListener('install', function(event){
     console.log('[SW] installing...');
-    event.waitUntil(caches.open('static_v2')
-        .then(function(cache){
-            console.log('[SW] precaching');
-            cache.addAll([
-                '/',
-                '/index.html'
-            ]);
-        }));
+    event.waitUntil(
+        caches.open(CACHE_STATIC_NAME)
+            .then(function(cache) {
+                cache.addAll(STATIC_CACHE_REF);
+        })
+    );
 });
 
 self.addEventListener('activate', function(event){
     console.log('[SW] activating...');
-    // LOOK THIS UP
+    event.waitUntil(
+        caches.keys()
+          .then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+              if (key !== CACHE_STATIC_NAME) {
+                return caches.delete(key);
+              }
+            }));
+          })
+    );
     self.clients.claim();
 });
 
